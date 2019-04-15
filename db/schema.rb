26676d5ff46232f9b98bf26abff16a8c602998fd
Samuel Beca <samuel.beca@gmail.com>
2019-04-15 13:27:39 +0200

    added fields model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_110201) do
+ActiveRecord::Schema.define(version: 2019_04_15_112618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "fields", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_fields_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +34,5 @@ ActiveRecord::Schema.define(version: 2019_04_15_110201) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "fields", "users"
 end
