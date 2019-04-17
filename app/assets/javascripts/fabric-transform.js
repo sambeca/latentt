889384f1363b79a731af40c1eeb8c29df745cc04
Samuel Beca <samuel.beca@gmail.com>
2019-04-17 20:55:36 +0200

    added more test images and changed fabricjs to bring each selected img to front

--- app/assets/javascripts/fabric-transform.js
+++ app/assets/javascripts/fabric-transform.js
@@ -1,18 +1,41 @@
 $( document ).ready(function() {
   window.canvas = new fabric.Canvas('canvas');
+  canvas.on("object:selected", function(options) {
+    options.target.bringToFront();
+});
 
-  fabric.Image.fromURL('https://openclipart.org/image/2400px/svg_to_png/224532/1438909128.png', function(img) {
-    img.scaleToWidth(100);
+  fabric.Image.fromURL('http://www.clker.com/cliparts/C/k/X/b/0/l/fuchsia-multilink-chain-hi.png', function(img) {
+    img.scaleToWidth(200);
+    img.set({ 'left': 100 });
+    img.set({ 'top': 100 });
     canvas.add(img);
   });
 
-  fabric.Image.fromURL('http://pluspng.com/img-png/water-droplet-png-hd-water-drop-transparent-png-563.png', function(img) {
-    img.scaleToWidth(100);
+  fabric.Image.fromURL('http://clipart-library.com/images/rTLyd4LT8.png', function(img) {
+    img.scaleToWidth(200);
+    img.set({ 'left': 200 });
+    img.set({ 'top': 200 });
     canvas.add(img);
   });
 
-  fabric.Image.fromURL('http://pluspng.com/img-png/rope-png-hd-climbing-rope-feral-designs-png-865.png', function(img) {
-    img.scaleToWidth(100);
+  fabric.Image.fromURL('http://moziru.com/images/mist-clipart-source-water-13.png', function(img) {
+    img.scaleToWidth(200);
+    img.set({ 'left': 300 });
+    img.set({ 'top': 300 });
+    canvas.add(img);
+  });
+
+  fabric.Image.fromURL('http://moziru.com/images/table-clipart-transparent-background-2.png', function(img) {
+    img.scaleToWidth(200);
+    img.set({ 'left': 400 });
+    img.set({ 'top': 400 });
+    canvas.add(img);
+  });
+
+   fabric.Image.fromURL('http://pre09.deviantart.net/6a3f/th/pre/i/2015/150/2/c/sphere_png_by_virgolinedancer1-d8vbbw4.png', function(img) {
+    img.scaleToWidth(200);
+    img.set({ 'left': 400 });
+    img.set({ 'top': 400 });
     canvas.add(img);
   });
 });
