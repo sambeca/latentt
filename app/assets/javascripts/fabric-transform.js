fb995ec1754069df34224786946bd3bf67cde629
Samuel Beca <samuel.beca@gmail.com>
2019-04-18 16:50:12 +0200

    resized images to fit across canvas

--- app/assets/javascripts/fabric-transform.js
+++ app/assets/javascripts/fabric-transform.js
@@ -6,68 +6,41 @@ $( document ).ready(function() {
     options.target.bringToFront();
   });
 
+  let percentage = 0.2;
+
   fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(300);
-    img.set({ 'left': 100 });
+    img.scaleToWidth(canvas.width*percentage);
+    img.set({ 'left': 0 });
     img.set({ 'top': 100 });
     canvas.add(img);
-    });
+  });
 
   fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(300);
-    img.set({ 'left': 200 });
-    img.set({ 'top': 200 });
+    img.scaleToWidth(canvas.width*percentage);
+    img.set({ 'left': 0.2 * canvas.width });
+    img.set({ 'top': 100 });
     canvas.add(img);
   });
 
   fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(300);
-    img.set({ 'left': 200 });
-    img.set({ 'top': 200 });
+    img.scaleToWidth(canvas.width*percentage);
+    img.set({ 'left': 0.4 * canvas.width });
+    img.set({ 'top': 100 });
     canvas.add(img);
   });
 
   fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(300);
-    img.set({ 'left': 200 });
-    img.set({ 'top': 200 });
+    img.scaleToWidth(canvas.width*percentage);
+    img.set({ 'left': 0.6 * canvas.width });
+    img.set({ 'top': 100 });
     canvas.add(img);
   });
 
   fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(300);
-    img.set({ 'left': 200 });
-    img.set({ 'top': 200 });
+    img.scaleToWidth(canvas.width*percentage);
+    img.set({ 'left': 0.8 * canvas.width });
+    img.set({ 'top': 100 });
     canvas.add(img);
   });
 });
-
-
-  // fabric.Image.fromURL('http://www.clker.com/cliparts/C/k/X/b/0/l/fuchsia-multilink-chain-hi.png', function(img) {
-  //   img.scaleToWidth(200);
-  //   img.set({ 'left': 100 });
-  //   img.set({ 'top': 100 });
-  //   canvas.add(img);
-  // });
-
-  // fabric.Image.fromURL('http://clipart-library.com/images/rTLyd4LT8.png', function(img) {
-  //   img.scaleToWidth(200);
-  //   img.set({ 'left': 200 });
-  //   img.set({ 'top': 200 });
-  //   canvas.add(img);
-  // });
-
-  // fabric.Image.fromURL('http://moziru.com/images/mist-clipart-source-water-13.png', function(img) {
-  //   img.scaleToWidth(200);
-  //   img.set({ 'left': 300 });
-  //   img.set({ 'top': 300 });
-  //   canvas.add(img);
-  // });
-
-  // fabric.Image.fromURL('http://moziru.com/images/table-clipart-transparent-background-2.png', function(img) {
-  //   img.scaleToWidth(200);
-  //   img.set({ 'left': 400 });
-  //   img.set({ 'top': 400 });
-  //   canvas.add(img);
-  // });
 
