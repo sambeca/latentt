91e5618bfa962cbf11ab506203434f33d3a09eb0
Samuel Beca <samuel.beca@gmail.com>
2019-04-19 13:50:46 +0200

    added randomization to size and left positioning

--- app/assets/javascripts/fabric-transform.js
+++ app/assets/javascripts/fabric-transform.js
@@ -6,7 +6,16 @@ $( document ).ready(function() {
     options.target.bringToFront();
   });
 
-  let percentage = 0.25;
+  const randomIntFromInterval = (min,max) => {
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+
+  const randomFloatFromInterval = (min, max) => {
+    return (Math.random() * (max - min) + min).toFixed(4);
+  }
+
+  let percentage = randomFloatFromInterval(0.2,0.3);
+
   let shadow = {
     color: '#888888',
     blur: 49,
@@ -15,144 +24,94 @@ $( document ).ready(function() {
     opacity: 0.1
   }
 
-  const randomIntFromInterval = (min,max) => {
-    return Math.floor(Math.random()*(max-min+1)+min);
-  }
-
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0 });
+    img.set({ 'left': randomFloatFromInterval(0,0.05) * canvas.width });
     img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.2 * canvas.width });
+    img.set({ 'left': randomFloatFromInterval(0.2,0.25) * canvas.width });
     img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.4 * canvas.width });
+    img.set({ 'left': randomFloatFromInterval(0.4,0.45) * canvas.width });
     img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.6 * canvas.width });
+    img.set({ 'left': randomFloatFromInterval(0.6,0.65) * canvas.width });
     img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.8 * canvas.width });
+    img.set({ 'left': randomFloatFromInterval(0.8,0.85) * canvas.width });
     img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0 });
-    img.set({ 'top': randomIntFromInterval(100,200) });
+    img.set({ 'left': randomFloatFromInterval(0,0.05) * canvas.width });
+    img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.2 * canvas.width });
-    img.set({ 'top': randomIntFromInterval(100,200) });
+    img.set({ 'left': randomFloatFromInterval(0.2,0.25) * canvas.width });
+    img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.4 * canvas.width });
-    img.set({ 'top': randomIntFromInterval(100,200) });
+    img.set({ 'left': randomFloatFromInterval(0.4,0.45) * canvas.width });
+    img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.6 * canvas.width });
-    img.set({ 'top': randomIntFromInterval(100,200) });
+    img.set({ 'left': randomFloatFromInterval(0.6,0.65) * canvas.width });
+    img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
+  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
     img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.8 * canvas.width });
-    img.set({ 'top': randomIntFromInterval(100,200) });
+    img.set({ 'left': randomFloatFromInterval(0.8,0.85) * canvas.width });
+    img.set({ 'top': randomIntFromInterval(0,100) });
     img.rotate(randomIntFromInterval(-30,30));
     img.setShadow(shadow);
     canvas.add(img);
   });
-
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0 });
-    img.set({ 'top': randomIntFromInterval(200,300) });
-    img.rotate(randomIntFromInterval(-30,30));
-    img.setShadow(shadow);
-    canvas.add(img);
-  });
-
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.2 * canvas.width });
-    img.set({ 'top': 100 });
-    img.rotate(randomIntFromInterval(200,300));
-    img.setShadow(shadow);
-    canvas.add(img);
-  });
-
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.4 * canvas.width });
-    img.set({ 'top': 100 });
-    img.rotate(randomIntFromInterval(200,300));
-    img.setShadow(shadow);
-    canvas.add(img);
-  });
-
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.6 * canvas.width });
-    img.set({ 'top': 100 });
-    img.rotate(randomIntFromInterval(200,300));
-    img.setShadow(shadow);
-    canvas.add(img);
-  });
-
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${Math.floor(Math.random() * 999)}-0.png`, function(img) {
-    img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': 0.8 * canvas.width });
-    img.set({ 'top': 100 });
-    img.rotate(randomIntFromInterval(200,300));
-    img.setShadow(shadow);
-    canvas.add(img);
-  });
-
 });
 
