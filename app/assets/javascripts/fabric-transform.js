5331e645182122db9d999dd7f5e95fdf96b2d278
Samuel Beca <samuel.beca@gmail.com>
2019-04-19 16:51:55 +0200

    added function to open url of selected image

--- app/assets/javascripts/fabric-transform.js
+++ app/assets/javascripts/fabric-transform.js
@@ -1,9 +1,17 @@
 $( document ).ready(function() {
   window.canvas = new fabric.Canvas('canvas');
+
   canvas.setWidth($('.canvas-container').width());
+
   canvas.setHeight($('.canvas-container').height());
-  canvas.on("object:selected", function(options) {
+
+  canvas.on("selection:updated", function(options) {
     options.target.bringToFront();
+    let selectedImgUrl = options.target.getSrc();
+    console.log(selectedImgUrl);
+    $("#openURL").on('click', function(){
+    window.open(selectedImgUrl, '_blank');
+    });
   });
 
   const randomIntFromInterval = (min,max) => {
@@ -107,13 +115,13 @@ $( document ).ready(function() {
     canvas.add(img);
   });
 
-  fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
-    img.scaleToWidth(canvas.width*percentage);
-    img.set({ 'left': randomFloatFromInterval(0.8,0.85) * canvas.width });
-    img.set({ 'top': randomIntFromInterval(0,verticalBoundry) });
-    img.rotate(randomIntFromInterval(-30,30));
-    img.setShadow(shadow);
-    canvas.add(img);
+    fabric.Image.fromURL(`https://patentimages.storage.googleapis.com/pages/US1102${randomIntFromInterval(0,999)}-0.png`, function(img) {
+      img.scaleToWidth(canvas.width*percentage);
+      img.set({ 'left': randomFloatFromInterval(0.8,0.85) * canvas.width });
+      img.set({ 'top': randomIntFromInterval(0,verticalBoundry) });
+      img.rotate(randomIntFromInterval(-30,30));
+      img.setShadow(shadow);
+      canvas.add(img);
   });
 });
 
