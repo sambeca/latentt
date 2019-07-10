c23af0e16e02db73e79c7a4df4528ecbf4ec59f2
Samuel Beca <samuel.beca@gmail.com>
2019-07-10 14:23:10 +0200

    add field create method adjust navbar styling

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,8 +1,18 @@
 class FieldsController < ApplicationController
-  before_action :set_phrase, only: [:show, :edit, :update, :destroy]
+  before_action :set_field, only: [:show, :edit, :update, :destroy]
 
   def index
     @fields = Field.all
+  end
+
+  def create
+    @field = Field.create(field_params)
+    @field.user_id = current_user.id
+    if @field.save
+      redirect_to fields_path, notice: "Saved"
+    else
+      render 'new', notice: "Something went wrong, we'll look into it"
+    end
   end
 
   def show
