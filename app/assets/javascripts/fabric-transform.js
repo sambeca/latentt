7d9740ff6341d90ef4485c03f8b6d648666c79f5
Samuel Beca <samuel.beca@gmail.com>
2019-07-10 17:46:01 +0200

    add conditional erb on navbar, change slider and input js

--- app/assets/javascripts/fabric-transform.js
+++ app/assets/javascripts/fabric-transform.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  // load canvas
+
   window.canvas = new fabric.Canvas("canvas");
 
   canvas.setWidth($(".canvas-container").width());
@@ -15,61 +17,87 @@ $(document).ready(function() {
     options.target.bringToFront();
   });
 
+  // open pages from selected
+
   const openCanvasImg = document.getElementById("openCanvasImg");
 
   openCanvasImg.addEventListener("click", () => {
     window.open(selectedImgUrl, "_blank");
   });
 
-  const openPagesFromSelected = document.getElementById("openPagesFromSelected");
+  const openPagesFromSelected = document.getElementById(
+    "openPagesFromSelected"
+  );
 
-openPagesFromSelected.addEventListener("click", () => {
-  page0Url = selectedImgUrl.slice(0, -5) + "0.png";
-  page1Url = selectedImgUrl.slice(0, -5) + "1.png";
-  page2Url = selectedImgUrl.slice(0, -5) + "2.png";
-  page3Url = selectedImgUrl.slice(0, -5) + "3.png";
-  page4Url = selectedImgUrl.slice(0, -5) + "4.png";
-  fabric.Image.fromURL(page0Url, function(img) {
-    img.scaleToWidth(canvas.width * percentage);
-    img.set({ left: randomFloatFromInterval(0, 0.05) * canvas.width });
-    img.set({ top: randomIntFromInterval(0, verticalBoundry) });
-    img.rotate(randomIntFromInterval(-30, 30));
-    img.setShadow(shadow);
-    canvas.add(img);
+  openPagesFromSelected.addEventListener("click", () => {
+    page0Url = selectedImgUrl.slice(0, -5) + "0.png";
+    page1Url = selectedImgUrl.slice(0, -5) + "1.png";
+    page2Url = selectedImgUrl.slice(0, -5) + "2.png";
+    page3Url = selectedImgUrl.slice(0, -5) + "3.png";
+    page4Url = selectedImgUrl.slice(0, -5) + "4.png";
+    fabric.Image.fromURL(page0Url, function(img) {
+      img.scaleToWidth(canvas.width * percentage);
+      img.set({ left: randomFloatFromInterval(0, 0.05) * canvas.width });
+      img.set({ top: randomIntFromInterval(0, verticalBoundry) });
+      img.rotate(randomIntFromInterval(-30, 30));
+      img.setShadow(shadow);
+      canvas.add(img);
+    });
+    fabric.Image.fromURL(page1Url, function(img) {
+      img.scaleToWidth(canvas.width * percentage);
+      img.set({ left: randomFloatFromInterval(0.2, 0.25) * canvas.width });
+      img.set({ top: randomIntFromInterval(0, verticalBoundry) });
+      img.rotate(randomIntFromInterval(-30, 30));
+      img.setShadow(shadow);
+      canvas.add(img);
+    });
+    fabric.Image.fromURL(page2Url, function(img) {
+      img.scaleToWidth(canvas.width * percentage);
+      img.set({ left: randomFloatFromInterval(0.4, 0.45) * canvas.width });
+      img.set({ top: randomIntFromInterval(0, verticalBoundry) });
+      img.rotate(randomIntFromInterval(-30, 30));
+      img.setShadow(shadow);
+      canvas.add(img);
+    });
+    fabric.Image.fromURL(page3Url, function(img) {
+      img.scaleToWidth(canvas.width * percentage);
+      img.set({ left: randomFloatFromInterval(0.6, 0.65) * canvas.width });
+      img.set({ top: randomIntFromInterval(0, verticalBoundry) });
+      img.rotate(randomIntFromInterval(-30, 30));
+      img.setShadow(shadow);
+      canvas.add(img);
+    });
+    fabric.Image.fromURL(page4Url, function(img) {
+      img.scaleToWidth(canvas.width * percentage);
+      img.set({ left: randomFloatFromInterval(0.8, 0.85) * canvas.width });
+      img.set({ top: randomIntFromInterval(0, verticalBoundry) });
+      img.rotate(randomIntFromInterval(-30, 30));
+      img.setShadow(shadow);
+      canvas.add(img);
+    });
   });
-  fabric.Image.fromURL(page1Url, function(img) {
-    img.scaleToWidth(canvas.width * percentage);
-    img.set({ left: randomFloatFromInterval(0.2, 0.25) * canvas.width });
-    img.set({ top: randomIntFromInterval(0, verticalBoundry) });
-    img.rotate(randomIntFromInterval(-30, 30));
-    img.setShadow(shadow);
-    canvas.add(img);
-  });
-  fabric.Image.fromURL(page2Url, function(img) {
-    img.scaleToWidth(canvas.width * percentage);
-    img.set({ left: randomFloatFromInterval(0.4, 0.45) * canvas.width });
-    img.set({ top: randomIntFromInterval(0, verticalBoundry) });
-    img.rotate(randomIntFromInterval(-30, 30));
-    img.setShadow(shadow);
-    canvas.add(img);
-  });
-  fabric.Image.fromURL(page3Url, function(img) {
-    img.scaleToWidth(canvas.width * percentage);
-    img.set({ left: randomFloatFromInterval(0.6, 0.65) * canvas.width });
-    img.set({ top: randomIntFromInterval(0, verticalBoundry) });
-    img.rotate(randomIntFromInterval(-30, 30));
-    img.setShadow(shadow);
-    canvas.add(img);
-  });
-  fabric.Image.fromURL(page4Url, function(img) {
-    img.scaleToWidth(canvas.width * percentage);
-    img.set({ left: randomFloatFromInterval(0.8, 0.85) * canvas.width });
-    img.set({ top: randomIntFromInterval(0, verticalBoundry) });
-    img.rotate(randomIntFromInterval(-30, 30));
-    img.setShadow(shadow);
-    canvas.add(img);
-  });
-});
+
+  // sliders
+
+  fieldMinSlider.oninput = function() {
+    localStorage.setItem("fieldMinInputLocal", this.value);
+    document.getElementById("fieldMinInput").value = this.value;
+  };
+
+  fieldMaxSlider.oninput = function() {
+    localStorage.setItem("fieldMaxInputLocal", this.value);
+    document.getElementById("fieldMaxInput").value = this.value;
+  };
+
+  let patentMinNum = JSON.parse(localStorage.getItem("fieldMinInputLocal"));
+  let patentMaxNum = JSON.parse(localStorage.getItem("fieldMaxInputLocal"));
+
+  document.getElementById("fieldMinSlider").value = patentMinNum;
+  document.getElementById("fieldMaxSlider").value = patentMaxNum;
+  document.getElementById("fieldMinInput").value = patentMinNum;
+  document.getElementById("fieldMaxInput").value = patentMaxNum;
+
+  // load images onto canvas
 
   const randomIntFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -81,10 +109,6 @@ openPagesFromSelected.addEventListener("click", () => {
 
   let percentage = randomFloatFromInterval(0.25, 0.35);
   let verticalBoundry = canvas.height - canvas.height * 0.6;
-
-  let patentMinNum = JSON.parse(document.getElementById("fieldMinInput").value);
-  let patentMaxNum = JSON.parse(document.getElementById("fieldMaxInput").value);
-
   let patentPageNum = 0;
 
   let shadow = {
